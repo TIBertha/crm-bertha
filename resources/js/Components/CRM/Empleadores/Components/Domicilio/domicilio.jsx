6d6c46cd7eb42ce430f9187c37c5e4721ecb5c70
Guillermo Pacherres <gerencia@bertha.pe>
2026-02-28 00:12:47 -0500

    sd

--- resources/js/Components/CRM/Empleadores/Components/Domicilio/domicilio.jsx
+++ resources/js/Components/CRM/Empleadores/Components/Domicilio/domicilio.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import DomicilioModal from 'domicilioModal.jsx';
+import DomicilioModal from './domicilioModal.jsx';
 
 export default function Domicilio({ domicilios, add, edit, delete: deleteItem }) {
     const [dataEdit, setDataEdit] = useState('');
