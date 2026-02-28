e6a4c58a64b6cb29f4e025a7f1c85a546afea213
Guillermo Pacherres <gerencia@bertha.pe>
2026-02-28 00:08:41 -0500

    mini problema

--- resources/js/Components/CRM/Requerimientos/Tabs/Postulaciones/postulaciones.jsx
+++ resources/js/Components/CRM/Requerimientos/Tabs/Postulaciones/postulaciones.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import Swal from "sweetalert2";
-import global from "../../../../helpers/constantes";
+import global from "../../../../Helpers/constantes.js";
 import {
     ajaxPostulacionesSelectWhatsapp,
     ajaxActionAddPostulante, ajaxListaPostulantesAdd, ajaxGetPostulaciones,
