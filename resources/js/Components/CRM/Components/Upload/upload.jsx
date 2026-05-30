6302301e63b79f6647d820b77df3dd12d2f61b5e
TIBertha <gerencia@bertha.pe>
2026-05-30 02:53:21 -0400

    asd

--- resources/js/Components/CRM/Components/Upload/upload.jsx
+++ resources/js/Components/CRM/Components/Upload/upload.jsx
@@ -21,11 +21,7 @@ export default class Upload extends Component {
         const { onUpload } = this.props;
 
         return (
-            <Dropzone accept={{
-                'image/jpeg': [],
-                'image/png': [],
-                'image/jpg': []
-            }} onDropAccepted={onUpload}>
+            <Dropzone accept={this.props.accept} onDropAccepted={onUpload}>
                 {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
                     <DropContainer
                         {...getRootProps()}
